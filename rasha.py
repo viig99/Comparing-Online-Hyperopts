59af5028dbb429c03f0e7a478494edf0e5036604
viig99 <accio.arjun@gmail.com>
2025-02-17 13:26:54 -0500

    Different inits.

--- rasha.py
+++ rasha.py
@@ -186,9 +186,9 @@ class LevelPromoter:
 class RandomAsynchronousSuccessiveHalvingAlgorithm(BaseOpt):
     def __init__(self, all_combinations: np.ndarray, **kwargs) -> None:
         super().__init__(all_combinations)
-        self.reduction_factor = 2
-        self.min_resource_r = 32
-        self.max_resource_r = 4096
+        self.reduction_factor = 4
+        self.min_resource_r = 4
+        self.max_resource_r = 2048
         self.early_stopping_rate = 0
 
         self.promoter = LevelPromoter(
