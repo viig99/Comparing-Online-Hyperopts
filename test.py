5dac702e3fa54d5eaa0db88d3b231a38a3077fbf
Arjun Variar <avariar@verticalscope.com>
2025-02-23 05:28:02 -0500

    Change distribution of reward to look more real world.

--- test.py
+++ test.py
@@ -23,15 +23,50 @@ class Config:
     num_items_shown: int = 25
     num_samples: list[int] = [100, 1000, 5000, 10000, 50000, 100000, 500000]
 
+    @property
+    def highest_possible_reward(self) -> float:
+        return sum(1/(i+1) for i in range(self.num_items_shown))
+
+    @property
+    def num_arms(self) -> int:
+        return int(np.prod([len(v) for v in self.hyperparameters.values()]))
+
+    def compute_rewards(self, hparam_combinations: np.ndarray) -> np.ndarray:
+        best_arm_idx = [np.random.randint(0, len(v)) for v in self.hyperparameters.values()]
+        best_values = np.array([v[best_arm_idx[i]] for i, v in enumerate(self.hyperparameters.values())])
+        stdevs = np.array([1.0, 0.05, 1.0, 1.0, 1.0, 1.0], dtype=float)
+        rho = np.array([
+            [1.0,  0.8,  0.0,  0.0,  0.0,  0.0],
+            [0.8,  1.0,  0.0,  0.0,  0.0,  0.0],
+            [0.0,  0.0,  1.0,  0.0,  0.0,  0.0],
+            [0.0,  0.0,  0.0,  1.0,  0.8,  0.5],
+            [0.0,  0.0,  0.0,  0.8,  1.0,  0.5],
+            [0.0,  0.0,  0.0,  0.5,  0.5,  1.0],
+        ])
+        Sigma = np.outer(stdevs, stdevs) * rho
+        Sigma_inv = np.linalg.inv(Sigma)
+        all_raw = []
+        for combo in hparam_combinations:
+            diff = combo - best_values
+            raw_score = 1 / (1 + (diff @ Sigma_inv @ diff))
+            all_raw.append(raw_score)
+        all_raw = np.array(all_raw)
+        max_raw = np.max(all_raw)
+        # Scale so the best combination gets "highest_val"
+        scaled_rewards = (all_raw / max_raw) * self.highest_possible_reward
+        return scaled_rewards
+
+    def compute_uniform_rewards(self, hparam_combinations: np.ndarray) -> np.ndarray:
+        return np.random.uniform(0, self.highest_possible_reward, size=len(hparam_combinations))
+
 def test():
     conf = Config()
 
     hparam_combinations = all_hparam_combinations(conf.hyperparameters)
-    num_arms = hparam_combinations.shape[0]
+    num_arms = conf.num_arms
     print(f"Number of arms: {num_arms}")
-    highest_possible_reward = sum(1/(i+1) for i in range(conf.num_items_shown))
 
-    reward_means = np.random.uniform(0, highest_possible_reward, size=num_arms)
+    reward_means = conf.compute_rewards(hparam_combinations)
 
     real_best_arm = np.argmax(reward_means)
     ranks = np.argsort(reward_means)[::-1]
